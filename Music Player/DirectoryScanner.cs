45a3a8d7ca7af659bcf360a4cbd5742261f51f66
SeventhSon <myselph.tom@gmail.com>
2013-11-03 15:35:43 +0100

    directory scanner skeleton

--- Music Player/DirectoryScanner.cs
+++ Music Player/DirectoryScanner.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Music_Player
 {
@@ -10,9 +11,10 @@ namespace Music_Player
     {
         private static volatile DirectoryScanner instance;
         private static object monitor = new Object();
+        private FileSystemWatcher FSW;
         private DirectoryScanner()
         {
-
+            //Initialize FileSystemWatcher
         }
         public static DirectoryScanner Instance
         {
@@ -28,6 +30,28 @@ namespace Music_Player
                 }
                 return instance;
             }
+        }
+        public int Scan(string path)
+        {
+            int MusicFilesFound = 0;
+            //After scanning add directory to FileSystemWatcher
+            return MusicFilesFound;
+        }
+        public int ScanRecursive(string path)
+        {
+            int MusicFilesFound = 0;
+            //After scanning add all directories to FileSystemWatcher
+            return MusicFilesFound;
+        }
+        public int GetLastWriteTime(string path)
+        {
+            int LastWriteTime = -1;
+            return LastWriteTime;
+        }
+        private static void OnFileChange()
+        {
+            //On change use DBManager to send infromation
+            //Later notify LibraryManager to refresh
         }
     }
 }
