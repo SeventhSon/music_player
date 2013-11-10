b378e9a596ecf5776cf43c0da0af6170f45723a1
SeventhSon <myselph.tom@gmail.com>
2013-11-10 09:59:48 +0100

    Added SongModel

---
+++ Music Player/Model/SongModel.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows.Controls;
+
+namespace Music_Player.Model
+{
+    class SongModel
+    {
+        public string Title { get; set; }
+        public string Artist { get; set; }
+        public string Album { get; set; }
+        public int Year { get; set; }
+        public string Genre { get; set; }
+        public int Length 
+        { 
+            get; 
+            set 
+            { 
+                Length = value;
+                LengthString = Length % 60 < 10 ? Length / 60 + ":0" + Length % 60 : Length / 60 + ":" + Length % 60;
+            } 
+        }
+        public string LengthString { get; set; }
+        public string Path { get; set; }
+        public string Genre { get; set; }
+        public int Rating { get; set; }
+        public Image AlbumArt { get; set; }
+        public int DirectoryID { get; set; }
+        public int TrackNo { get; set; }
+    }
+}
